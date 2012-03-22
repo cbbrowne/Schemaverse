7147d2ce697ebdd22733b1e253bdd32cb41d7b38
Christopher Browne <cbbrowne@acm.org>
2012-03-22 10:05:58 -0400

    Switch to dollar quoting

--- sample_script.sql
+++ sample_script.sql
@@ -1,10 +1,10 @@
-update my_fleets set script = '
+update my_fleets set script = $$
 
 -- Seize planets
 perform mine(ship, planet) from planets_in_range pr, planets p
 where pr.planet = p.id and p.conqueror_id <> 2663;
 
-update planets set name = ''FunbusterLand'' where id in (select planet from planets_in_range) and conqueror_id <> (select id from my_player);
+update planets set name = 'FunbusterLand' where id in (select planet from planets_in_range) and conqueror_id <> (select id from my_player);
 
 -- Move scouts towards their desired locations
 drop table if exists scout_locations;
@@ -48,9 +48,9 @@ insert into speediness (ship_id, want_speed)
   select id, (2000-max_speed)/20+5 from my_ships where fleet_id = 231 and max_speed < 1995 and current_health > 0
   order by random() limit 5;
 
-perform convert_resource(''FUEL'', coalesce((select sum(want_speed) from speediness),0)::integer);
+perform convert_resource('FUEL', coalesce((select sum(want_speed) from speediness),0)::integer);
 
-perform upgrade(ship_id, ''MAX_SPEED'', ship_id) from speediness;
+perform upgrade(ship_id, 'MAX_SPEED', ship_id) from speediness;
 
 drop table if exists want_ships;
 create temp table want_ships (fleet_id integer, name text, attack integer, defense integer, engineering integer, prospecting integer, location_x integer, location_y integer);
@@ -60,21 +60,21 @@ if (select fuel_reserve from my_player) > 5000 then
 
    -- Build a scout and a prospector
    insert into want_ships (fleet_id, name, attack, defense, engineering, prospecting,location_x,location_y)
-      select 231, ''Scout'', 5,4,4,7, p.location_x, p.location_y from planets p where conqueror_id = 231 
+      select 231, 'Scout', 5,4,4,7, p.location_x, p.location_y from planets p where conqueror_id = 231 
       order by random() limit 1;
 
    insert into want_ships (fleet_id, name, attack, defense, engineering, prospecting,location_x,location_y)
-      select 234, ''Miner'', 0,2,2,16, p.location_x, p.location_y from planets p where conqueror_id = 231 
+      select 234, 'Miner', 0,2,2,16, p.location_x, p.location_y from planets p where conqueror_id = 231 
       order by random() limit 1;
 
 elsif ((select fuel_reserve from my_player) > 1500) then
    if random() > 0.8 then
      insert into want_ships (fleet_id, name, attack, defense, engineering, prospecting,location_x,location_y)
-       select 231, ''Scout'', 5,4,4,7, p.location_x, p.location_y from planets p where conqueror_id = 231 
+       select 231, 'Scout', 5,4,4,7, p.location_x, p.location_y from planets p where conqueror_id = 231 
        order by random() limit 1;
    else
      insert into want_ships (fleet_id, name, attack, defense, engineering, prospecting,location_x,location_y)
-       select 234, ''Miner'', 4,2,1,13, p.location_x, p.location_y from planets p where conqueror_id = 231 
+       select 234, 'Miner', 4,2,1,13, p.location_x, p.location_y from planets p where conqueror_id = 231 
        order by random() limit 1;
    end if;
 
@@ -83,7 +83,7 @@ elsif ((select fuel_reserve from my_player) > 1500) then
         from want_ships;
 end if;
 
-perform convert_resource(''FUEL'', (select count(*) from want_ships)::integer * 1000);
+perform convert_resource('FUEL', (select count(*) from want_ships)::integer * 1000);
 
 insert into my_ships (fleet_id, name, attack, defense, engineering, prospecting, location_x, location_y) 
       select fleet_id, name, attack, defense, engineering, prospecting, location_x, location_y
@@ -94,7 +94,7 @@ create temp table directed_scouts (ship_id integer, planet_id integer);
 insert into directed_scouts (ship_id, planet_id)
   select s.id as ship_id, p.id as planet_id 
     from my_ships s, planets p, my_fleets f 
-    where s.fleet_id = f.id and f.name = ''Scouts'' and (p.location <->s.destination) < 1;
+    where s.fleet_id = f.id and f.name = 'Scouts' and (p.location <->s.destination) < 1;
 
 drop table if exists undirected_scouts;
 create temp table undirected_scouts (ship_id integer);
@@ -102,7 +102,7 @@ create temp table undirected_scouts (ship_id integer);
 insert into undirected_scouts (ship_id)
 select s.id from my_ships s, my_fleets f 
 where 
- f.id = s.fleet_id and f.name = ''Scouts'' and
+ f.id = s.fleet_id and f.name = 'Scouts' and
  (destination is null or exists (select 1 from planets p where (p.location <-> s.destination) < 10 and conqueror_id = (select id from my_player)));
 
 drop table if exists possible_destinations;
@@ -137,20 +137,20 @@ perform attack(r.id, r.ship_in_range_of) from my_ships s, ships_in_range r where
 drop table if exists attack_targets;
 create temp table attack_targets (cause text, location point, as_at timestamptz);
 insert into attack_targets (cause, location, as_at)
-   select ''RECENT CONQUER'', location, toc from my_events
-   where action = ''CONQUER'' and toc > (now() - ''8 hours''::interval);
+   select 'RECENT CONQUER', location, toc from my_events
+   where action = 'CONQUER' and toc > (now() - '8 hours'::interval);
 insert into attack_targets (cause, location, as_at)
-   select ''RECENT ATTACK'', location, toc from my_events
-   where action = ''ATTACK'' and toc > (now() - ''8 hours''::interval);
+   select 'RECENT ATTACK', location, toc from my_events
+   where action = 'ATTACK' and toc > (now() - '8 hours'::interval);
 insert into attack_targets (cause, location, as_at)
-   select ''IN RANGE'', enemy_location, now()
+   select 'IN RANGE', enemy_location, now()
    from ships_in_range;
 
 drop table if exists attack_missions;
 
 
 
-' where name = 'Scouts';
+$$ where name = 'Scouts';
 
 
 -- update my_fleets set enabled = 't' where name = 'Scouts';
